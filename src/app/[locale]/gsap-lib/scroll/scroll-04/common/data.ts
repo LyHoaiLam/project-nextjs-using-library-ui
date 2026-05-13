3942e6db2468704d6cef0d6f568d38d34be465f1
Lý Hoài Lâm <hoailam03999@gmail.com>
2026-05-14 05:30:31 +0700

    add animation button-navigation-bar

---
+++ src/app/[locale]/gsap-lib/scroll/scroll-04/common/data.ts
@@ -0,0 +1,39 @@
+import { SectionData } from "../page"
+
+export const sectionsData: SectionData[] = [
+  {
+    id: "first",
+    navLabel: "One",
+    title: "City Skyline",
+    className: "first",
+    bgUrl: "/images/bg-one.jpg",
+  },
+  {
+    id: "second",
+    navLabel: "Two",
+    title: "Flowers of Friendship",
+    className: "second",
+    bgUrl: "/images/bg-two.jpg",
+  },
+  {
+    id: "third",
+    navLabel: "Three",
+    title: "Waves in the Ocean",
+    className: "third",
+    bgUrl: "/images/bg-three.jpg",
+  },
+  {
+    id: "fourth",
+    navLabel: "Four",
+    title: "New York City",
+    className: "fourth",
+    bgUrl: "/images/bg-four.jpg",
+  },
+  {
+    id: "fifth",
+    navLabel: "Five",
+    title: "Dark Side of the Moon",
+    className: "fifth",
+    bgUrl: "/images/bg-five.jpg",
+  },
+]
